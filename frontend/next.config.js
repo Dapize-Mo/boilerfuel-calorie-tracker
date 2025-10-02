3253867a2efd283a8b08b81a0931106017f233e3
Dapize-Mo <oolaniy@purdue.edu>
2025-10-02 11:39:28 -0400

    Fix vercel.json syntax and next.config.js env variable

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:5000',
-    NEXT_PUBLIC_BYPASS_AUTH: process.env.NEXT_PUBLIC_BYPASS_AUTH || false,
+    NEXT_PUBLIC_BYPASS_AUTH: process.env.NEXT_PUBLIC_BYPASS_AUTH || 'false',
   },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
